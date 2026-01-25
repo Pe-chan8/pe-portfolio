f38762f98cef3e1dcf00cd95a681592aa06b7d42
Pe-chan8 <hr.tp.892837@gmail.com>
2026-01-25 17:12:31 +0900

    Disable css_compressor in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,5 @@ Rails.application.configure do
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   config.assets.compile = false
   config.assets.digest = true
+  config.assets.css_compressor = nil
 end
